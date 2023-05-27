9078bcc1834c63b375fa6faa8c35b00a8381cabf
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2023-05-27 15:19:26 -0400

    styling

--- App.js
+++ App.js
@@ -1,24 +1,31 @@
+import * as Permissions from 'expo-permissions';
 import { Camera, CameraType } from 'expo-camera';
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import { styles } from "./styles/Style";
 
 export default function App() {
   const [startCamera,setStartCamera] = React.useState(false)
   const [type, setType] = useState(CameraType.back);
   const [status, setStatus] = Camera.useCameraPermissions();
 
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      setStatus(status);
+    };
+
+    getCameraPermissions();
+  }, []);
+
   const goBack = () => {
     setStartCamera(false);
   }
 
   const takePic = function (){
-    
-    if (status.granted) {
-      setStartCamera(true);
-    } else {
-      // Handle permission denied scenario
-    }
+    setStartCamera(true); // 💀💀💀💀
   }
 
   const analyzeText = () => {
@@ -28,60 +35,27 @@ export default function App() {
   return (
     <View style={styles.container}>
       {startCamera ? (
-        <Camera  style={{flex: 1,width:"100%"}}
+        <Camera  style={styles.camera}
         ref={(r) => {
           camera = r
-        }} type={type}>
-        <TouchableOpacity style={{
-              width: 130,
-              borderRadius: 4,
-              backgroundColor: '#14274e',
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: 40
-            }} onPress={goBack}>
-            <Text style={{
-                color: '#fff',
-                fontWeight: 'bold',
-                textAlign: 'center'
-              }}>Back</Text>
+        }} type={type}> 
+        <TouchableOpacity style={styles.button} onPress={goBack}>
+            <Text style={styles.text}>Back</Text>
           </TouchableOpacity>
 
           <TouchableOpacity
             onPress={analyzeText}
-            style={{
-            position: 'absolute',
-            margin: 'auto',
-            alignSelf: 'center',
-            width: 70,
-            height: 70,
-            bottom: 25,
-            borderRadius: 50,
-            backgroundColor: '#fff'
-            }}
+            style={styles.cameraButton}
             />
       </Camera>
       ) :
       (
         <><Text>Hihi magic placeholder text something something 🙃🙃🙃🙃🙃🙃🙃🙃🙃🙃</Text><TouchableOpacity
             onPress={takePic}
-            style={{
-              width: 130,
-              borderRadius: 4,
-              backgroundColor: '#14274e',
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: 40
-            }}
+            style={styles.button}
           >
             <Text
-              style={{
-                color: '#fff',
-                fontWeight: 'bold',
-                textAlign: 'center'
-              }}
+              style={styles.text}
             >
               Scan receipt
             </Text>
@@ -92,11 +66,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
