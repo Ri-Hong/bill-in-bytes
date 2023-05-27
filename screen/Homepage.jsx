c951100a32049bc1904365cc0de063180debc063
broskiboy <jefferson.chen2@student.tdsb.on.ca>
2023-05-27 19:37:44 -0400

    updated homepage html

--- screen/Homepage.jsx
+++ screen/Homepage.jsx
@@ -7,75 +7,75 @@ export default function Homepage() {
             <View style={styles.user_container}>
                 <Image source={require('./my-icon.png')} style={styles.user_image} /> 
                 <View style={styles.user_text_container}>
-                    <Text style={styles.subheader_text}> Hey! </Text>
-                    <Text style={styles.header_text}> Jefferson Chen </Text>
+                    <Text style={styles.subheader_text}>Hey!</Text>
+                    <Text style={styles.header_text}>Jefferson Chen</Text>
                 </View>
             </View>
 
             <View style={styles.stats_container}>
-                <Text style={styles.text_header}> Your Stats</Text>
+                <Text style={styles.text_header}>Your Stats</Text>
                 <View style={styles.container}>
-                    <Text style={styles.stats_money}> $25,402.49 </Text>
-                    <Text style={styles.stats_your_spendings}> Your Spendings </Text>
+                    <Text style={styles.stats_money}>$25,402.49</Text>
+                    <Text style={styles.stats_your_spendings}>Your Spendings</Text>
                 </View>
                 <View style={styles.timing_container}>
                     <View style={styles.box_container}>
                         <Image source={require('./my-icon.png')} style={styles.image_style}/> 
-                        <Text style={styles.number}> 125 </Text>
-                        <Text style={styles.text_under}> Total </Text>
+                        <Text style={styles.number}>125</Text>
+                        <Text style={styles.text_under}>Total</Text>
                     </View>
 
                     <View style={styles.box_container}>
                         <Image source={require('./my-icon.png')} style={styles.image_style}/> 
-                        <Text style={styles.number}> 123 </Text>
-                        <Text style={styles.text_under}> Resolved </Text>
+                        <Text style={styles.number}>123</Text>
+                        <Text style={styles.text_under}>Resolved</Text>
                     </View>
 
                     <View style={styles.box_container}>
                         <Image source={require('./my-icon.png')} style={styles.image_style}/> 
-                        <Text style={styles.number}> 2 </Text>
-                        <Text style={styles.text_under}> Unresolved </Text>
+                        <Text style={styles.number}>2</Text>
+                        <Text style={styles.text_under}>Unresolved</Text>
                     </View>
                 </View>
             </View>
 
             <View style={styles.transactions_container}>
-                <Text style={styles.text_header}>  Transactions </Text>
+                <Text style={styles.text_header}>Transactions</Text>
                 
 
                 <View>
                     <View style={styles.transactions_text_container}>
                         <Image source={require('./my-icon.png')} style={styles.transactions_image}/>
-                        <Text style={styles.dior_text}> Dior </Text>
-                        <Text style={styles.transactions_date}> June 11, 2023 </Text>
-                        <Text style={styles.transactions_money}> $124.50 </Text>
+                        <Text style={styles.dior_text}>Dior</Text>
+                        <Text style={styles.transactions_date}>June 11, 2023</Text>
+                        <Text style={styles.transactions_money}>$124.50</Text>
                     </View>
                     
                     <Hr></Hr>
 
                     <View style={styles.transactions_text_container}>
                         <Image source={require('./my-icon.png')} style={styles.transactions_image}/>
-                        <Text style={styles.dior_text}> Dior </Text>
-                        <Text style={styles.transactions_date}> June 11, 2023 </Text>
-                        <Text style={styles.transactions_money}> $124.50 </Text>
+                        <Text style={styles.dior_text}>Dior</Text>
+                        <Text style={styles.transactions_date}>June 11, 2023</Text>
+                        <Text style={styles.transactions_money}>$124.50</Text>
                     </View>
                     
                     <Hr></Hr>
                     
                     <View style={styles.transactions_text_container}>
                         <Image source={require('./my-icon.png')} style={styles.transactions_image}/> 
-                        <Text style={styles.dior_text}> Dior </Text>
-                        <Text style={styles.transactions_date}> June 11, 2023 </Text>
-                        <Text style={styles.transactions_money}> $124.50 </Text>
+                        <Text style={styles.dior_text}>Dior</Text>
+                        <Text style={styles.transactions_date}>June 11, 2023</Text>
+                        <Text style={styles.transactions_money}>$124.50</Text>
                     </View>
 
                     <Hr></Hr>
 
                     <View style={styles.transactions_text_container}>
                         <Image source={require('./my-icon.png')} style={styles.transactions_image}/>
-                        <Text style={styles.dior_text}> Dior </Text>
-                        <Text style={styles.transactions_date}> June 11, 2023 </Text>
-                        <Text style={styles.transactions_money}> $124.50 </Text>
+                        <Text style={styles.dior_text}>Dior</Text>
+                        <Text style={styles.transactions_date}>June 11, 2023</Text>
+                        <Text style={styles.transactions_money}>$124.50</Text>
                     </View>
                 </View>
             </View>
