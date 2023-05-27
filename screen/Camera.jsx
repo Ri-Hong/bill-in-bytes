cc9318cd589cb140f8c0a4495cee615bc6a3d2ed
broskiboy <jefferson.chen2@student.tdsb.on.ca>
2023-05-27 17:28:25 -0400

    landing html

--- screen/Camera.jsx
+++ screen/Camera.jsx
@@ -4,9 +4,8 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import { styles } from "./styles/Style";
 
-export default function App() {
+export default function CameraScreen() {
   const [startCamera,setStartCamera] = React.useState(false)
   const [type, setType] = useState(CameraType.back);
   const [status, setStatus] = Camera.useCameraPermissions();
@@ -66,3 +65,36 @@ export default function App() {
   );
 }
 
+const styles = StyleSheet.create({
+  button: {
+      width: 130,
+      borderRadius: 4,
+      backgroundColor: '#14274e',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 40
+  },
+  camera: {flex: 1, width:"100%"},
+  cameraButton: {
+      position: 'absolute',
+      margin: 'auto',
+      alignSelf: 'center',
+      width: 70,
+      height: 70,
+      bottom: 25,
+      borderRadius: 50,
+      backgroundColor: '#fff'
+  },
+  container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  text: {
+      color: '#fff',
+      fontWeight: 'bold',
+      textAlign: 'center'
+  },
+});
