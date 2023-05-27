13994adef15b14b1ba213d1665c205e9a82d77e7
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2023-05-27 14:59:06 -0400

    added buttons

--- App.js
+++ App.js
@@ -1,39 +1,78 @@
 import * as Permissions from 'expo-permissions';
-import { Camera } from 'expo-camera';
+import { Camera, CameraType } from 'expo-camera';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   const [startCamera,setStartCamera] = React.useState(false)
+  const [type, setType] = useState(CameraType.back);
+  const [status, setStatus] = Camera.useCameraPermissions();
 
-  const takePic = async function (){
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    if (status === 'granted') {
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      setStatus(status);
+    };
+
+    getCameraPermissions();
+  }, []);
+
+  const goBack = () => {
+    setStartCamera(false);
+  }
+
+  const takePic = function (){
+    
+    if (status.granted) {
+      // setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
       setStartCamera(true);
     } else {
       // Handle permission denied scenario
     }
-    // setStartCamera(true)
-    // const {status} = await Camera.getCameraPermissionsAsync()
-    // if(status === 'granted'){
-    //   // do something
-    //   setStartCamera(true)
-    // }else{
-    //   Alert.alert("Access denied")
-    // }
   }
-  
+
+  const analyzeText = () => {
+
+  }
 
   return (
     <View style={styles.container}>
       {startCamera ? (
-        <Camera
-        style={{flex: 1,width:"100%"}}
+        <Camera  style={{flex: 1,width:"100%"}}
         ref={(r) => {
           camera = r
-        }}
-      ></Camera>
+        }} type={type}>
+        <TouchableOpacity style={{
+              width: 130,
+              borderRadius: 4,
+              backgroundColor: '#14274e',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 40
+            }} onPress={goBack}>
+            <Text style={{
+                color: '#fff',
+                fontWeight: 'bold',
+                textAlign: 'center'
+              }}>Back</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={analyzeText}
+            style={{
+            position: 'absolute',
+            margin: 'auto',
+            alignSelf: 'center',
+            width: 70,
+            height: 70,
+            bottom: 25,
+            borderRadius: 50,
+            backgroundColor: '#fff'
+            }}
+            />
+      </Camera>
       ) :
       (
         <><Text>Hihi magic placeholder text something something 🙃🙃🙃🙃🙃🙃🙃🙃🙃🙃</Text><TouchableOpacity
