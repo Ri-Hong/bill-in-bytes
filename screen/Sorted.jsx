41228f0d8019c83f8801a38799c1afef175fdcc9
broskiboy <jefferson.chen2@student.tdsb.on.ca>
2023-05-28 15:22:05 -0400

    sorted button

--- screen/Sorted.jsx
+++ screen/Sorted.jsx
@@ -24,15 +24,10 @@ export default function Sorted({navigation}) {
                 <Text style={styles.total}>$124.50</Text>
             </View>
 
-            <View style={styles.nav_bar}>  
-                <Pressable onPress={() => navigation.navigate("Homepage")}><Image source={NavHome} style={styles.nav_icon}/></Pressable>
-                <Image source={NavCalendar} style={styles.nav_icon}/>
-                <View style={styles.camera_container}>
-                    <Pressable onPress={() => navigation.navigate("CameraScreen")}><Image source={NavScan} style={styles.nav_icon}/></Pressable>    
-                </View>
-                <Pressable onPress={() => navigation.navigate("History")}><Image source={NavHistory} style={styles.nav_icon}/></Pressable> 
-                <Pressable onPress={() => navigation.navigate("Profile")}><Image source={NavProfile} style={styles.nav_icon}/></Pressable>
-
+            <View style={styles.button_container}>
+                <Pressable style={styles.returnButton} onPress={() => navigation.navigate("CameraScreen")}><Image source={backArrow} style={styles.returnArrow}/></Pressable>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("Sorting")}><Text style={styles.buttonText}>Continue</Text></Pressable>
+                
             </View>
         </SafeAreaView>
         
@@ -150,29 +145,6 @@ export default function Sorted({navigation}) {
         fontSize: 20,
         fontFamily: "Rokkitt",
 
-    },
-
-    nav_bar: {
-        display: 'flex',
-        flexDirection: 'row',
-        position: "absolute",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        justifyContent: "space-between",
-        paddingHorizontal: 30,
-        paddingBottom: 50,
-        alignItems: 'flex-end',
-        borderTopWidth: 1,
-        borderTopColor: "#f1f1f1",
-        height: 100,
-        backgroundColor: "#16191D"
-    },
-
-    nav_icon: {
-        width: 30,
-        height: 30,
-        resizeMode: 'contain',
     },
 
 
